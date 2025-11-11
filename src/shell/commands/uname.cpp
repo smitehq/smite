3421bd7fdef2cf80572860e97fa46638d03562c9
Sean W <seanwragg@gmail.com>
2025-11-11 16:04:18 -0500

    remove unused

--- src/shell/commands/uname.cpp
+++
@@ -1,7 +0,0 @@
-// register_command("uname", [this](const auto& args) -> std::string {
-//         bool all = !args.empty() && args[0] == "-a";
-//         if (all) {
-//             return "Linux lappy486 6.7.0-smite #1 SMP Thu Nov 7 2025 x86_64 GNU/Linux\n";
-//         }
-//         return "Linux\n";
-//     });
